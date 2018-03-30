154499bf9ae045d2a9a10b802a954c4f027fcf20
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2018-03-29 21:48:28 -0600

    Added generate tile and detect neighbour
    Also started working on rotate tile function but rotating the open tile placeholders is harder.

--- Capstone The Path of Love/Assets/Scripts/GameManager.cs
+++ Capstone The Path of Love/Assets/Scripts/GameManager.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿// =============================================
+// Notes
+// - Current tile will always be last placed tile regardless of where the player is
+// - had to put in positions around tile in the function not in the Tile class to work (won't get correct position until after the function is done)
+// =============================================
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,18 +18,21 @@ public class GameManager : MonoBehaviour {
 
 	public Tile start;
 	public Tile currentTile;
-	public List<Tile> nextTile;
+	public List<Tile> nextTiles;
+	public Tile prevTile;
 
 	public int numberInPool = 10;
-	public List<Tile> cornerTiles; // pool of tiles
-	public List<Tile> straightTiles;
-	public List<Tile> crossTiles;
+	int numberOfTileTypes = 3;
+	List<Tile> cornerTiles; // pool of tiles
+	List<Tile> straightTiles;
+	List<Tile> crossTiles;
 
-	// placeholder tiles
-	public int numberOfOpen = 4;
+	// pool of placeholder tiles
+	public int numberOfOpen = 20;
 	List<Tile> openTiles;
 
-	float tileSize = 1f; // length of one edge
+	public float tileSize = 1f; // length of one edge
+	public Vector3 north, south, east, west; // positions around the tile
 
 	// Use this for initialization
 	void Start () {
@@ -57,17 +66,18 @@ public class GameManager : MonoBehaviour {
 
 		// For beginning tile show active edges
 		showTilePlaces(start);
+		currentTile = start;
+		prevTile = start;
 	}
-		
+
 	public void showTilePlaces (Tile currTile){
 		// show possible positions of next tile based on current tile type
-		Debug.Log("Current tile shown:"+currTile.tileType);
-
+		//set the positions around the tile
 		// written so that the positions are in respect to the current rotation
-		Vector3 north = currTile.transform.position + tileSize*currTile.transform.forward;
-		Vector3 south = currTile.transform.position - tileSize*currTile.transform.forward;
-		Vector3 east = currTile.transform.position + tileSize*transform.TransformPoint(Vector3.right);
-		Vector3 west = currTile.transform.position + tileSize*transform.TransformPoint(Vector3.left);
+		north = currTile.transform.position + tileSize*currTile.transform.forward;
+		south = currTile.transform.position - tileSize*currTile.transform.forward;
+		east = currTile.transform.position + tileSize*currTile.transform.TransformPoint(Vector3.right);
+		west = currTile.transform.position + tileSize*currTile.transform.TransformPoint(Vector3.left);
 
 		// start tile has 4 open edges
 		if (currTile.tileType == "start") {
@@ -80,15 +90,24 @@ public class GameManager : MonoBehaviour {
 		// straight tile has 1 open edge
 		// chose a random edge and rotation will make sure that it is free
 		if (currTile.tileType == "straight") {
+			Debug.Log ("showing open tiles for straight tile");
+			Debug.Log ("current tile in showTile:"+currTile);
+			Debug.Log ("current north" + north);
 			MakeTile (openTiles, north);
 		}
 
 		// corner tile has 2 possible open edges
 		// but only one of them is open at a time.
+		// Walls assumed to be at north and east.
 		// for now have two edges
 		if (currTile.tileType == "corner") {
-			MakeTile (openTiles, west);
-			MakeTile (openTiles, south);
+			bool southIsOccupied = DetectNeighbourTile (south);
+
+			if (southIsOccupied) {
+				MakeTile (openTiles, west);
+			} else {
+				MakeTile (openTiles, south);
+			}
 		}
 	}
 
@@ -97,8 +116,13 @@ public class GameManager : MonoBehaviour {
 		// only applies to open tiles
 		// set the next tile into the clicked position
 		//get next generated tile here TBD
-		List<Tile> nextTiles = cornerTiles;
-		MakeTile(nextTiles, tileClicked.transform.position);
+		prevTile = currentTile;
+		Debug.Log ("previous tile: " + prevTile);
+
+//		GenerateNextTile ();
+//		MakeTile(nextTiles, tileClicked.transform.position); //sets new current tile
+		MakeTile(straightTiles, tileClicked.transform.position);
+		Debug.Log ("current tile in Settile: " + currentTile);
 		tileClicked.gameObject.SetActive (false); // must be before showTilePlaces so there will be tiles in the pool
 		showTilePlaces (currentTile);
 	}
@@ -112,13 +136,65 @@ public class GameManager : MonoBehaviour {
 				tileList [i].transform.rotation = Quaternion.identity;
 				tileList [i].gameObject.SetActive(true);
 
-				currentTile = tileList [i];
+				if (tileList [i].tileType != "open") {
+					currentTile = tileList [i];
+					Debug.Log ("current tile: " + currentTile);
+				}
 
 				break;
 			}
 		}
 	}
 
-	// generate next tile
+	public bool DetectNeighbourTile ( Vector3 position ){
+		//detect if there is already a tile in the indicated direction
+
+		// is there an open placeholder tile?
+		for (int i = 0; i < openTiles.Count; i++) {
+			if (openTiles [i].gameObject.transform.position == position) {
+				return true;
+			}
+		}
+
+		// get position of previous tile and compare to current tile
+		Vector3 occupiedPosition = prevTile.gameObject.transform.position;
+		if (occupiedPosition == position) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+		
+		
+	public void GenerateNextTile(){
+		// generate next tile type
+
+		int randomNumber = Random.Range (1, numberOfTileTypes);
+
+		if (randomNumber == 1) {
+			nextTiles = cornerTiles;
+		} else if (randomNumber == 2) {
+			nextTiles = straightTiles;
+		} else if (randomNumber == 3) {
+			nextTiles = crossTiles;
+		}
+	}
+
+//	public void RotateTile(Tile tileClicked){
+		//rotates the clicked tile, and any associated open tiles.
+//		tileClicked.transform.rotation = Quaternion.Euler (0, 90, 0);
+
+		// if it's a corner piece, it should turn off the open tile placeholder and reshow it
+//		for (int i = 0; i < openTiles.Count; i++) {
+//			if (openTiles [i].gameObject.transform.position == tileClicked.south) {
+//				openTiles [i].gameObject.SetActive (false);
+//			}
+//			else if (openTiles [i].gameObject.transform.position == tileClicked.north) {
+//				openTiles [i].gameObject.SetActive (false);
+//			}
+//		}
+//		showTilePlaces (tileClicked);
+//	}
 }
 	
--- Capstone The Path of Love/Assets/Scripts/Tile.cs
+++ Capstone The Path of Love/Assets/Scripts/Tile.cs
@@ -6,9 +6,11 @@ public class Tile : MonoBehaviour {
 
 	public string tileType = "cross";
 
+	public void Start(){
+	}
+
 	public void setTileType (string tt){
 		// set tile type to "corner", "straight", "cross", or "open"
 		tileType = tt;
 	}
-
 }
